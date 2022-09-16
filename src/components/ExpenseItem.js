394206f353b346e998522f2daf8bd2d26acca652
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-16 14:31:46 +0200

    Added initial files!

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css';
+function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+      <div>March 28th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>246 dollars</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;

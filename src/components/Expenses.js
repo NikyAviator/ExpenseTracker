92855b8ada5e52270202d0aff0b5d40fbcf8ee37
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-19 17:55:16 +0200

    Expenses component added!

---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+export default function Expenses(props) {
+  return (
+    <div>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </div>
+  );
+}

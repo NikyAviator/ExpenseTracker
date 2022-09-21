e154970b2f5f573681a830786a8482d7881c8dba
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-21 19:51:50 +0200

    small grammar changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,13 +8,15 @@ function ExpenseItem(props) {
 
   // stateless component / presentationall/ dumb component
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>{props.amount}$</div>
-      </div>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>{props.amount}$</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 export default ExpenseItem;

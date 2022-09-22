a3916a9b0857fed8e977f6a140ed15dfc0b22cff
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-22 13:40:54 +0200

    Added max values and spread them out to each month

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,3 +1,27 @@
 import React from 'react';
 
-const Chart = (props) => {};
+import ChartBar from './ChartBar';
+import './Chart.css';
+
+// List of content dynamically
+// we are defining dataPoint later and attaching a value.
+const Chart = (props) => {
+  // we transform a dataPoint object to just a number, the number stored in dataPoint.value
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  // pull out array with ... and add them as stand-alone arguments
+  const totalMaximum = Math.max(...dataPointValues);
+  return (
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
